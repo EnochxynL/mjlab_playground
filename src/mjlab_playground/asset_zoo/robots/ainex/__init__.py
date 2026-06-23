ea2baed532dccb46558f841f42746c9c5510329c
Enochxyn <Enochxyn@outlook.com>
2026-06-23 20:29:02 +0800

    init

---
+++ src/mjlab_playground/asset_zoo/robots/ainex/__init__.py
@@ -0,0 +1,6 @@
+"""Hiwonder AiNex humanoid."""
+
+from mjlab_playground.asset_zoo.robots.ainex.ainex_constants import (
+    AINEX_ACTION_SCALE,  # noqa: F401
+    get_ainex_robot_cfg,  # noqa: F401
+)
